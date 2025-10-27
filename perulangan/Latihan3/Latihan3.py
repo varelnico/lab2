107a55c6a8b442da435e8d5a1208a3ddcbc09c6e
Reaverr_ <niconico7876@gmail.com>
2025-10-28 01:49:19 +0700

    Update Latihan3.py

--- perulangan/Latihan3/Latihan3.py
+++ perulangan/Latihan3/Latihan3.py
@@ -1,1 +1,7 @@
-
+jawab = 'ya'
+hitung = 0
+while jawab.lower() == 'ya':
+    hitung += 1
+    print("Perulangan ke-", hitung)
+    jawab = input("Ulang lagi tidak? (ya/tidak): ")
+print("Total perulangan:", hitung)
