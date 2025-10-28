dbc1808db0257097c91ab16ec3a8629f6f7dea9e
Reaverr_ <niconico7876@gmail.com>
2025-10-28 11:55:07 +0700

    Delete perulangan/Latihan3 directory

--- perulangan/Latihan3/Latihan3.py
+++
@@ -1,7 +0,0 @@
-jawab = 'ya'
-hitung = 0
-while jawab.lower() == 'ya':
-    hitung += 1
-    print("Perulangan ke-", hitung)
-    jawab = input("Ulang lagi tidak? (ya/tidak): ")
-print("Total perulangan:", hitung)
